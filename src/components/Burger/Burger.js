cb21668841b59fbbdb120e97dced31e5b9ab3c00
Prakash Baskaran <b.prakashboss@gmail.com>
2021-02-02 08:19:01 +1300

    finished till section 127

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,17 +1,32 @@
-import React from 'react'
-import classes from './Burger.css'
+import React from "react";
+import classes from "./Burger.css";
 import Burgeringredient from "./Burgeringredient/Burgeringredient";
 
 function burger(props) {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      // console.log(igKey)
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <Burgeringredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  console.log(transformedIngredients);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!!</p>;
+  }
+
+  
   return (
     <div className={classes.Burger}>
-      <Burgeringredient type='bread-top'/>
-      <Burgeringredient type='cheese'/>
-      <Burgeringredient type='meat'/>
-      <Burgeringredient type='bread-bottom'/>
-      
+      <Burgeringredient type="bread-top" />
+      {transformedIngredients}
+      <Burgeringredient type="bread-bottom" />
     </div>
-  )
+  );
 }
 
-export default burger
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,22 @@ import Burger from '../../components/Burger/Burger'
 import Aux from "../../hoc/auxi";
 
 class BurgerBuilder extends Component {
+  // constructor (props) {
+  //   super(props)
+  //   this.state = {}
+  // }
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
   render() {
     return (
       <Aux>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Burger Controls</div>
       </Aux>
     );
